39974fcba7ebc0dcc2e59953725b224065527d2a
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-25 15:06:39 +0000

    change

--- head.js
+++ head.js
@@ -8,7 +8,7 @@ const assertEqual = function(actual, expected) {
   let head = function (array){
       let firstElement = array.shift();
       return firstElement;
-  }
+  };
   assertEqual(head([5,6,7]), 5);
   assertEqual(head([]), 5);
   assertEqual(head([5,6,7]), );
