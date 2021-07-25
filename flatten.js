1a78fa821c2916d2f85e63c144e667cac3e4976a
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-25 15:02:29 +0000

    modify

--- flatten.js
+++ flatten.js
@@ -1,3 +1,19 @@
+const eqArrays = (arrX, arrY) =>
+ arrX.length === arrY.length &&
+ arrX.every((x, y) => x === arrY[y]);
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+     console.log(`✅ Assertion Passed: Array 1 === Array 2 👍`);
+  } else if(eqArrays(arr1, arr2) === false) {
+    console.log(`⛔ Assertion Failed: Array 1  ❗== Array 2 ❌`)
+  }
+};
+
+const eqArrays = (arrX, arrY) => arrX.length === arrY.length
+ && arrX.every((x, y) => x === arrY[y]);
+
+
 let flatten = arry => arry.flat();
 
 //console.log(flatten([1, [4, 3, 2,], 2, [3, 4], 5, [6]]));
