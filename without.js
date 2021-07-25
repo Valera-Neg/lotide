815d8bfe5e6fba44e82017ee0e59aa57a8e0afdc
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-25 15:09:28 +0000

    without

--- without.js
+++ without.js
@@ -1,3 +1,16 @@
+const eqArrays = (arrX, arrY) =>
+ arrX.length === arrY.length &&
+ arrX.every((x, y) => x === arrY[y]);
+
+const assertArraysEqual = (arr1, arr2) => {
+  if (eqArrays(arr1, arr2) === true) {
+     console.log(`✅ Assertion Passed: Array 1 === Array 2 👍`);
+  } else if(eqArrays(arr1, arr2) === false) {
+    console.log(`⛔ Assertion Failed: Array 1  ❗== Array 2 ❌`)
+  }
+};
+
+
 const without = (source, itemsToRemove) => {
   let unmached = [];
   for(let i = 0; i < source.length; i++){
@@ -12,3 +25,9 @@ const without = (source, itemsToRemove) => {
   }
    return unmached;
 };
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+
+assertArraysEqual(words, ["hello", "World", "lighthouse"]);
+console.log(without(words, ["lighthouse"]));
