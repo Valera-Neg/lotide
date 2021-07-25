6cdcb89d8a8ddd0c6d00eab20656c3b231edc256
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-25 14:59:02 +0000

    some chenges

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,13 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
-const eqArrays = (arrX, arrY) =>
- arrX.length === arrY.length &&
- arrX.every((x, y) => x === arrY[y]);
+const eqArrays = (arrX, arrY) => arrX.length === arrY.length && arrX.every((x, y) => x === arrY[y]);
+
+  const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+      console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
+    } else {
+      console.log(`🛑 Assertion Failed: ${actual} !=== ${expected}`);
+    }
+   };
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 0, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),  false);
+assertEqual(eqArrays([1, 2, 3], [1, 0, 3]), true);
