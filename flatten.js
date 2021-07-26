d90f9327756632677354984920a5d022b264cc95
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-26 03:38:46 +0000

    better

--- flatten.js
+++ flatten.js
@@ -1,7 +1,3 @@
-const eqArrays = (arrX, arrY) =>
- arrX.length === arrY.length &&
- arrX.every((x, y) => x === arrY[y]);
-
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2) === true) {
      console.log(`✅ Assertion Passed: Array 1 === Array 2 👍`);
@@ -16,4 +12,6 @@ const eqArrays = (arrX, arrY) => arrX.length === arrY.length
 
 let flatten = arry => arry.flat();
 
-//console.log(flatten([1, [4, 3, 2,], 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten(['Val', ['Matt', 'Tim'], 'Zev', ['Hello']]),['Val', 'Matt', 'Tim', 'Zev', 'Hello']);
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([9, 7, [false, 5], 5, [8]]),[9, 7, 3, 5, 5, 8]);
