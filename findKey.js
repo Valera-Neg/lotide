72dfc2cf5cdff81d52a7eb41e02f8325fbfc78c0
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-07-30 00:11:38 +0000

    fined first key in object

---
+++ findKey.js
@@ -0,0 +1,41 @@
+const findKey = function(obj, callback) {
+for (let [key,value] of Object.entries(obj)) {
+    if (callback(value)) {
+      return key;
+    } 
+  }
+ return undefined;
+};
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !=== ${expected}`);
+  }
+};
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3), "Akaleri");
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 1), "Blue Hill");
