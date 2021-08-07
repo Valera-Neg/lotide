0dde26ca88550b8a1569e02ee28ffe8c61dbfa3e
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-07 13:29:52 +0000

    small changes

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,20 @@
 const eqArrays = (arrX, arrY) =>
  arrX.length === arrY.length &&
- arrX.every((x, y) => x === arrY[y]);
+ arrX.every((elem, i) => elem == arrY[i]);
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2) === true) {
-     console.log(`✅ Assertion Passed: Array 1 === Array 2 👍`);
+     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2} 👍`);
   } else if(eqArrays(arr1, arr2) === false) {
-    console.log(`⛔ Assertion Failed: Array 1  ❗== Array 2 ❌`)
+    console.log(`⛔⛔⛔ Assertion Failed: ${arr1}  ❗== ${arr2} ❌`)
   }
 };
 
 
 let a = ['Val', 3, "Peter", false, "false", "3"];
-let b = ['Val', 1, "Peter", false, "false", "3"];
+let b = ['Val', 3, "Peter", false, "false", "3"];
 assertArraysEqual(a,b);
+
+let c = ['Val', 3, "Peter", true, "false", "3"];
+let d = ['Val', 3, "Peter", true, "false", "3"];
+assertArraysEqual(c, d);
