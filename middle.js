8f875a95893e882c4772a17e59fb835a5233e56c
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-08 17:27:21 +0000

    test separation

--- middle.js
+++ middle.js
@@ -1,16 +1,4 @@
-const eqArrays = (arrX, arrY) =>
- arrX.length === arrY.length &&
- arrX.every((x, y) => x === arrY[y]);
-
-const assertArraysEqual = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2) === true) {
-     console.log(`✅ Assertion Passed: Array 1 === Array 2 👍`);
-  } else if(eqArrays(arr1, arr2) === false) {
-    console.log(`⛔ Assertion Failed: Array 1  ❗== Array 2 ❌`)
-  }
-};
-
-let middle = arr => {
+const middle = arr => {
   let newArr = [];
   if(arr.length < 3) {
     return newArr;
@@ -22,15 +10,7 @@ let middle = arr => {
         return newArr;  
       }
 };
-
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, ]), [5]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9, ]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 3, 6, 7, 8, 9, ]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 4]);
-assertArraysEqual(middle(['ant', 'bison', 'camel', 'duck', 'elephant']),  ["camel"]);
-assertArraysEqual(middle(["bison", "camel", "duck", "elephant"]),  ["camel", "duck"]);
-
+module.exports = middle;
 
 
 
