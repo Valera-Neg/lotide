25513cb20922a6ce7a7f15f9acf9f776ef79ebe1
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-08 17:03:53 +0000

    after test separation

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,4 @@
-const eqArrays = (arrX, arrY) =>
- arrX.length === arrY.length &&
- arrX.every((elem, i) => elem == arrY[i]);
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2) === true) {
@@ -9,12 +7,4 @@ const assertArraysEqual = (arr1, arr2) => {
     console.log(`⛔⛔⛔ Assertion Failed: ${arr1}  ❗== ${arr2} ❌`)
   }
 };
-
-
-let a = ['Val', 3, "Peter", false, "false", "3"];
-let b = ['Val', 3, "Peter", false, "false", "3"];
-assertArraysEqual(a,b);
-
-let c = ['Val', 3, "Peter", true, "false", "3"];
-let d = ['Val', 3, "Peter", true, "false", "3"];
-assertArraysEqual(c, d);
+module.exports = assertArraysEqual;
