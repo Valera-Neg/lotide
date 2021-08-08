b2354a4c87914ce3722aa2be6ea24c44736ef7ba
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-08 15:17:45 +0000

    after test separation

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,2 @@
 const eqArrays = (arrX, arrY) => arrX.length === arrY.length && arrX.every((x, y) => x === arrY[y]);
-
-  const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
-    } else {
-      console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-   };
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [1, 0, 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, false, 3]),  false);
-assertEqual(eqArrays([1, 2, 3], [1, true, 3]), false);
-assertEqual(eqArrays(["Karl","Salima","Agouhanna","Fang"], ["Karl","Salima","Agouhanna","Fang"]), true);
+module.exports = eqArrays;
