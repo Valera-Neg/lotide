0d6e7ded3fc229f821195bddf12b602e151ab440
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-08 14:08:44 +0000

    after test  sepatation

--- head.js
+++ head.js
@@ -1,17 +1,6 @@
-const assertEqual = function(actual, expected) {
-    if (actual === expected) {
-      console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
-    } else {
-      console.log(`🛑 Assertion Failed: ${actual} !=== ${expected}`);
-    }
+let head = function (array){
+  let firstElement = array.shift();
+  return firstElement;
   };
-  let head = function (array){
-      let firstElement = array.shift();
-      return firstElement;
-  };
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head([]), 5);
-  assertEqual(head([5,6,7]), );
-  assertEqual(head(["Lighthouse", "Labs"]), "Hello");
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), );
+  
+  module.exports = head;
