82c81ff6b35eb9d13583dacd1f91187d464d07bc
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-08 15:05:07 +0000

    after test separation

--- tail.js
+++ tail.js
@@ -1,14 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${ actual} === ${ expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !=== ${expected}`);
-  }
-};
 let tail = function(words) {
   let tailOfArr = words.slice(1);
   return tailOfArr;
-};  
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); 
-assertEqual(words.length, 3);
+}; 
+module.exports = tail;
