1cbe12d84d10c2c2bc329cb1edbf4e92b8c2b1e7
Valeriy Nehovora <valeriynegovora@gmail.com>
2021-08-10 00:17:06 +0000

    more

---
+++ index.js
@@ -0,0 +1,32 @@
+const head = require('./head');
+const tail = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const countOnly = require('./countOnly');
+const countLetters = require('./countLetters');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  eqObjects,
+  countOnly,
+  countLetters,
+  findKey,
+  findKeyByValue,
+  flatten,
+  letterPositions,
+  map,
+  takeUntil,
+  without
+};
